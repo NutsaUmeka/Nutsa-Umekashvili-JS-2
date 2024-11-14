b573cde31ac78c55de0f2751cc6f1219dac51872
NutsaUmeka <nuciacule@gmail.com>
2024-11-15 01:42:42 +0400

    js

---
+++ script.js
@@ -0,0 +1,30 @@
+//1
+let number = 19
+if (number * number > 100) {
+    console.log("number1's square is more than 100");
+    
+} else {
+    console.log("number1's square is not more than 100");
+    
+}
+
+//2
+let number2 = 67 ;
+
+if (50 < number < 100) {
+    console.log("number2 is between 50 and 100");
+    
+} else {
+    console.log("number2 is not between 50 and 100");
+    
+}
+
+//3
+let number3 = 12;
+if (number2 <= 0) {
+    console.log("number3 is zero or negative");
+    
+} else {
+    console.log("number3 is not zero or negative");
+    
+}
